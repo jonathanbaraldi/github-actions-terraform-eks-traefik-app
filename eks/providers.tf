2df64a2381f920ecb723eb706ed829b234e4cd9b
ghustavof <ghustavo.teodoro@gmail.com>
2021-11-29 19:25:28 +0000

    Parameterized the files according to my environment

--- eks/providers.tf
+++ eks/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-deploy-cluster-rke"
+    bucket = "terraform-deploy-cluster-eks"
     key    = "devops-cluster-eks"
     region = "us-east-2"
   }
--- eks/vpc.tf
+++ eks/vpc.tf
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "devops-cluster-rke-vpc"
+  name                 = "devops-cluster-eks-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
