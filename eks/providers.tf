380a0692a7a417ce11fe02ce2ae0baa85a2d12cd
ghustavof <ghustavo.teodoro@gmail.com>
2021-11-29 18:25:52 +0000

    Parameterized the files according to my environment

--- eks/providers.tf
+++ eks/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-deploy-jonjon"
-    key    = "devops-ninja-eks"
+    bucket = "terraform-deploy-cluster-rke"
+    key    = "devops-cluster-eks"
     region = "us-east-2"
   }
 }
