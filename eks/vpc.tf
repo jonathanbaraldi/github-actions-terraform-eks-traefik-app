e677c7a5018e846b3b35026831a325a75ba77876
mario.dias <mario.dias@movti.com.br>
2022-11-25 23:21:58 -0400

    alteracao nome vpc e cluster

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "devops-ninja-eks-${random_string.suffix.result}"
+  cluster_name = "dev-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "devops-ninja-vpc"
+  name                 = "dev-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
